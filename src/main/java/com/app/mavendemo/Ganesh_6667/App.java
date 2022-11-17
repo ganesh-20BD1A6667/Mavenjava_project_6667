b4db012b9fd0e97cca2323dbbaca356fc8e68544
ganesh-20BD1A6667 <20bd1a6667@gmail.com>
2022-11-17 20:34:24 +0530

    javademo

---
+++ src/main/java/com/app/mavendemo/Ganesh_6667/App.java
@@ -0,0 +1,13 @@
+package com.app.mavendemo.Ganesh_6667;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
